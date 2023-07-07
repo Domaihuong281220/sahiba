f68dbe77ddeb5715b21da5ae8d514868b3505f62
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2023-07-07 14:39:32 +0700

    Tay-NV Update

--- sahiba/src/App.js
+++ sahiba/src/App.js
@@ -3,7 +3,7 @@
 import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Carousel from "./app/components/Carousel/Carousel";
+// import Carousel from "./app/components/Carousel/Carousel";
 import Footer from "./app/components/Footer/Footer";
 import Header from "./app/components/Header/Header";
 
@@ -13,13 +13,13 @@ import imageFreeShipping from "./assets/image/payments/shipping-min.webp";
 import imageDesign from "./assets/image/payments/1000+design-min.webp";
 import CardImage from "./app/components/CardImage/CardImage";
 
-//shop by type
+// //shop by type
 import imageCard_1 from "./assets/image/shop-by-weaving-type/hand-tufted-min.webp";
 import imageCard_2 from "./assets/image/shop-by-weaving-type/hand-woven-min.webp";
 import imageCard_3 from "./assets/image/shop-by-weaving-type/hand-knotted-min.webp";
 import imageCard_4 from "./assets/image/shop-by-weaving-type/flat-weave-min.webp";
 
-//Shop by style
+// //Shop by style
 import imageCard_5 from "./assets/image/shop-by-style/classic.jpg";
 import imageCard_6 from "./assets/image/shop-by-style/traditional.jpg";
 import imageCard_7 from "./assets/image/shop-by-style/transitional.jpg";
@@ -31,7 +31,7 @@ import imageCard_12 from "./assets/image/shop-by-style/natural.jpg";
 import imageCard_13 from "./assets/image/shop-by-style/kilim.jpg";
 import imageCard_14 from "./assets/image/shop-by-style/texture.jpg";
 
-//shop By color
+// //shop By color
 import img1 from "./assets/image/shop-by-color/beige.jpg";
 import img2 from "./assets/image/shop-by-color/blue-rugs.jpg";
 import img3 from "./assets/image/shop-by-color/black-rugs.jpg";
@@ -187,7 +187,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Carousel autoSlide={true} />
+      {/* <Carousel autoSlide={true} />
       <div className=" grid grid-cols-4 w-full h-48 ">
         <div className="flex flex-col items-center justify-center">
           <img className="object-fit w-24 h-24" src={imagePayment}></img>
@@ -207,7 +207,7 @@ function App() {
         </div>
       </div>
       {/* start shop by weaving type */}
-      <div className="w-full py-4">
+      {/* <div className="w-full py-4">
         <p className=" text-3xl font-medium pb-10 font-mei">
           Shop By Weaving Type
         </p>
@@ -216,10 +216,10 @@ function App() {
             return <CardImage title={title} url={url} />;
           })}
         </div>
-      </div>
+      </div> */}
 
       {/* start shop by style */}
-      <div className="w-full py-4 ">
+      {/* <div className="w-full py-4 ">
         <p className=" text-3xl font-medium pb-10 font-mei">Shop By Style</p>
         <div className="flex justify-center flex-wrap gap-x-3 gap-y-6  ">
           {cardDataShopByStyle.map(({ title, url }) => {
@@ -235,7 +235,7 @@ function App() {
         {dataColor.map(({ title, url }) => {
           <CarouselByColor title={title} url />;
         })}
-      </div>
+      </div>  */}
 
       <Footer />
     </div>
