7e0170d06b69dead941982bb8dd53f221d004c34
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2023-07-11 14:27:34 +0700

    HuongDM

--- sahiba/src/Pages/Homepage/Homepage.jsx
+++ sahiba/src/Pages/Homepage/Homepage.jsx
@@ -2,9 +2,20 @@
 import "./style.css";
 
 import img1 from "../../assets/image/Homepage/img1.jpg";
-// import { Button, Carousel, Typography } from "@material-tailwind/react";
-import Carousel from "../../app/components/Carousel/Carousel";
+import imgslider from "../../assets/image/Homepage/Saree.jpg";
+import { Carousel, Col, Row } from 'antd';
+import React from "react";
 
+const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
+
+
+const contentStyle: React.CSSProperties = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 const Homepage = () => {
   return (
     <>
@@ -21,124 +32,117 @@ const Homepage = () => {
         <div className="Head-intro2 font-mar">
           <h1>Since 1990</h1>
           <p>Located on the banks of the Ganges on the ancient Grand Trunk Route, Obeetee is the largest handmade rug maker in India. But there’s more to us than our craft. Garden parties and silver service for a century - A charming way of life is kept alive at The Bungalow. Meet the company behind the product.
-          Located on the banks of the Ganges on the ancient Grand Trunk Route, Obeetee is the largest handmade rug maker in India. But there’s more to us than our craft. Garden parties and silver service for a century - A charming way of life is kept alive at The Bungalow. Meet the company behind the product.
-          Located on the banks of the Ganges on the ancient Grand Trunk Route, Obeetee is the largest handmade rug maker in India. But there’s more to us than our craft. Garden parties and silver service for a century - A charming way of life is kept alive at The Bungalow. Meet the company behind the product.
+            Located on the banks of the Ganges on the ancient Grand Trunk Route, Obeetee is the largest handmade rug maker in India. But there’s more to us than our craft. Garden parties and silver service for a century - A charming way of life is kept alive at The Bungalow. Meet the company behind the product.
+            Located on the banks of the Ganges on the ancient Grand Trunk Route, Obeetee is the largest handmade rug maker in India. But there’s more to us than our craft. Garden parties and silver service for a century - A charming way of life is kept alive at The Bungalow. Meet the company behind the product.
           </p>
           <a href="https://www.obeetee.com/" target="_blank" rel="noreferrer">OUR LEGACY</a>
         </div>
 
         {/* Part 3 */}
-        {/* <Carousel className="rounded-xl">
-      <div className="relative h-full w-full">
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-          <div className="w-3/4 text-center md:w-2/4">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-            >
-              The Beauty of Nature
-            </Typography>
-            <Typography
-              variant="lead"
-              color="white"
-              className="mb-12 opacity-80"
-            >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that
-              quality of air that emanation from old trees, that so
-              wonderfully changes and renews a weary spirit.
-            </Typography>
-            <div className="flex justify-center gap-2">
-              <Button size="lg" color="white">
-                Explore
-              </Button>
-              <Button size="lg" color="white" variant="text">
-                Gallery
-              </Button>
+        <div className="Head-intro3">
+          
+
+          <Carousel autoplay>
+            <div>
+            <Row gutter={16}>
+            <Col className="gutter-row" span={24}>
+              <img src={imgslider} alt="Logo" border="0" className="logo" />
+            </Col>
+            <Col className="row2 gutter-row" span={24}>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={8}>
+                  <h1 className="font-mei">Sarees</h1>
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <p className="font-mar">A celebrated revisit to Shantanu & Nikhil’s iconic collections over the years and what the brand stands for. Power, Progressiveness and Pride now takes centre stage and finds its way into the luxurious world of Obeetee’s handcrafted carpets.</p>
+
+                </Col>
+                <Col className="gutter-row" span={8}>
+                  
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <a className="font-mar" href="https://www.obeetee.com/" target="_blank" rel="noreferrer">SHOP NOW</a>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="relative h-full w-full">
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
-          <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-            >
-              The Beauty of Nature
-            </Typography>
-            <Typography
-              variant="lead"
-              color="white"
-              className="mb-12 opacity-80"
-            >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that
-              quality of air that emanation from old trees, that so
-              wonderfully changes and renews a weary spirit.
-            </Typography>
-            <div className="flex gap-2">
-              <Button size="lg" color="white">
-                Explore
-              </Button>
-              <Button size="lg" color="white" variant="text">
-                Gallery
-              </Button>
+            <div>
+            <Row gutter={16}>
+            <Col className="gutter-row" span={24}>
+              <img src={imgslider} alt="Logo" border="0" className="logo" />
+            </Col>
+            <Col className="row2 gutter-row" span={24}>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={8}>
+                  <h1 className="font-mei">Sarees</h1>
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <p className="font-mar">A celebrated revisit to Shantanu & Nikhil’s iconic collections over the years and what the brand stands for. Power, Progressiveness and Pride now takes centre stage and finds its way into the luxurious world of Obeetee’s handcrafted carpets.</p>
+                </Col>
+                <Col className="gutter-row" span={8}>
+                  
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <a className="font-mar" href="https://www.obeetee.com/" target="_blank" rel="noreferrer">SHOP NOW</a>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="relative h-full w-full">
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-        <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
-          <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-            >
-              The Beauty of Nature
-            </Typography>
-            <Typography
-              variant="lead"
-              color="white"
-              className="mb-12 opacity-80"
-            >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that
-              quality of air that emanation from old trees, that so
-              wonderfully changes and renews a weary spirit.
-            </Typography>
-            <div className="flex gap-2">
-              <Button size="lg" color="white">
-                Explore
-              </Button>
-              <Button size="lg" color="white" variant="text">
-                Gallery
-              </Button>
+            <div>
+            <Row gutter={16}>
+            <Col className="gutter-row" span={24}>
+              <img src={imgslider} alt="Logo" border="0" className="logo" />
+            </Col>
+            <Col className="row2 gutter-row" span={24}>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={8}>
+                  <h1 className="font-mei">Sarees</h1>
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <p className="font-mar">A celebrated revisit to Shantanu & Nikhil’s iconic collections over the years and what the brand stands for. Power, Progressiveness and Pride now takes centre stage and finds its way into the luxurious world of Obeetee’s handcrafted carpets.</p>
+                </Col>
+                <Col className="gutter-row" span={8}>
+                  
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <a className="font-mar" href="https://www.obeetee.com/" target="_blank" rel="noreferrer">SHOP NOW</a>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
             </div>
-          </div>
+            <div>
+            <Row gutter={16}>
+            <Col className="gutter-row" span={24}>
+              <img src={imgslider} alt="Logo" border="0" className="logo" />
+            </Col>
+            <Col className="row2 gutter-row" span={24}>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={8}>
+                  <h1 className="font-mei">Sarees</h1>
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <p className="font-mar">A celebrated revisit to Shantanu & Nikhil’s iconic collections over the years and what the brand stands for. Power, Progressiveness and Pride now takes centre stage and finds its way into the luxurious world of Obeetee’s handcrafted carpets.</p>
+                </Col>
+                <Col className="gutter-row" span={8}>
+                  
+                </Col>
+                <Col className=" col2-row2 gutter-row" span={16}>
+                  <a className="font-mar" href="https://www.obeetee.com/" target="_blank" rel="noreferrer">SHOP NOW</a>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+            </div>
+          </Carousel>
+
         </div>
-      </div>
-     </Carousel> */}
-     <Carousel/>
       </div>
     </>
   );
