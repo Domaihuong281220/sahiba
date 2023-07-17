fd8eb66927941bf5784b9c619cd4e29af669c96c
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2023-07-17 09:45:45 +0700

    Tay-NV updata Media-Page

--- sahiba/src/app/pages/media-page/MediaPage.js
+++ sahiba/src/app/pages/media-page/MediaPage.js
@@ -10,19 +10,34 @@ import media_03 from "../../../assets/image/media_03.mp4";
 const MediaPage = () => {
   return (
     <>
-      <div className="grid grid-cols-3 py-20">
+      <div className="grid grid-cols-3 py-20 ">
         <div className="flex justify-center pt-3">
-          <video className="h-[500px] w-10/12 rounded-lg" controls autoPlay>
+          <video
+            className="h-[300px] w-10/12 rounded-lg"
+            controls
+            autoPlay
+            muted="true"
+          >
             <source src={media_01} type="video/mp4" />
           </video>
         </div>
         <div className="flex justify-center pt-3">
-          <video className="h-[500px] w-10/12 rounded-lg" controls autoPlay>
+          <video
+            className="h-[300px] w-10/12 rounded-lg"
+            controls
+            autoPlay
+            muted="true"
+          >
             <source src={media_02} type="video/mp4" />
           </video>
         </div>
         <div className="flex justify-center pt-3">
-          <video className="h-[500px] w-10/12 rounded-lg" controls autoPlay>
+          <video
+            className="h-[300px] w-10/12 rounded-lg"
+            controls
+            autoPlay
+            muted="true"
+          >
             <source src={media_03} type="video/mp4" />
           </video>
         </div>
